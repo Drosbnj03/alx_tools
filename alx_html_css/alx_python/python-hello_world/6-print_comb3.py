a399a46eefb37025e860a5587b68ea4fb44e225b
Achraf <achrafdrs03@outlook.com>
2023-12-14 23:15:24 +0100

    5

---
+++ alx_html_css/alx_python/python-hello_world/6-print_comb3.py
@@ -0,0 +1,7 @@
+for i in range(0,10):
+    for j in range(i+1,10):
+        if i!=j:
+            if str(i)+str(j) =='89':
+                print(89)
+            else:    
+                print('{}{}'.format(i,j),end = ", " )
